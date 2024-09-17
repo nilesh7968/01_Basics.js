d8b372821372fd9405b8628305ad180ca488db40
nilesh7968 <124447439+nilesh7968@users.noreply.github.com>
2024-09-17 18:56:24 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 2344556
+let accountemail = "nilesh@gmail.com"
+var accountPassword = "jbuguuyuy677"
+accountCity = "Kolkata"
+let accountState;
+
+//accountId = 2 // not allowed
+
+
+let accountEmail = "nil@gmail.com"
+var accountPassword = "j77"
+accountCity = "Bihar"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in bloack scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
